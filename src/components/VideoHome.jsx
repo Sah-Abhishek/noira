3ca4eea495940a9b4798349c5ff218a432f2c191
sah-abhishek <abhisheksah3333@gmail.com>
2025-08-08 16:45:49 +0530

    edited video section

--- src/components/VideoHome.jsx
+++ src/components/VideoHome.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function AutoPlayVideo() {
   return (
     <div className="bg-black text-white py-8 px-4 text-center">
-      <h2 className="text-3xl text-yellow-500 font-bold  mb-6">Welcome Experience</h2>
+      <h2 className="text-3xl text-yellow-500 font-bold  mb-6">At Home Treatment</h2>
 
       <div className='flex mt-20 mx-30' >
         <video
